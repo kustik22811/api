e7c412232615195259439d77c37ddf3e5cb8d25d
kustik22811 <65448537+kustik22811@users.noreply.github.com>
2021-03-13 23:26:38 +0800

    Update config.js

--- kran/без данных/config.js
+++ kran/без данных/config.js
@@ -1,9 +1,9 @@
 module.exports = {
 
     db: '', /* MongoDB connection link */
-    secretKey: '', /* VK Mini Apps service secret key */
+    secretKey: '81jTSajfNBMiosfRTCIn', /* VK Mini Apps service secret key */
     user_id: 0, /* VK user id */
     token: '', /* VK access token */
-    key: '' // VK Coin merchant key
+    key: 'a1bb6651a1bb6651a1bb665157a1cd5041aa1bba1bb6651c1acc37575aee806ad8f9a4a' // VK Coin merchant key
 
 };
