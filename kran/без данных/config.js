dd79050d1f4a26274b2c563a160cb9c9c0e5e321
kustik22811 <65448537+kustik22811@users.noreply.github.com>
2021-03-13 23:33:15 +0800

    Delete kran directory

--- kran/без данных/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-    db: '', /* MongoDB connection link */
-    secretKey: '81jTSajfNBMiosfRTCIn', /* VK Mini Apps service secret key */
-    user_id: 0, /* VK user id */
-    token: '', /* VK access token */
-    key: 'a1bb6651a1bb6651a1bb665157a1cd5041aa1bba1bb6651c1acc37575aee806ad8f9a4a' // VK Coin merchant key
-
-};
